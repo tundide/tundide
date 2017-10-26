39925c0be0a840ab058be129a30efc5970f61c8d
MarcosPanichella <30484139+MarcosPanichella@users.noreply.github.com>
2017-10-26 12:22:56 -0300

    Se cambia el modal por un form para ppointments new

--- src/admin/appointment/appointment.new.component.ts
+++ src/admin/appointment/appointment.new.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
 import { PhonebookService } from '../phonebook/phonebook.service';
 import { Appointment } from './appointment.model';
@@ -16,11 +17,10 @@ import * as moment from 'moment';
 })
 
 export class AppointmentNewComponent implements OnInit {
-    @Output()
-    changeAppointment: EventEmitter<Appointment> = new EventEmitter<Appointment>();
-
-    private appointment: Appointment;
     private roles: Array<String>;
+    private appointment: Appointment;
+    private appointmentGroup: FormGroup;
+
     private datepickerOpts = {
         assumeNearbyYear: true,
         autoclose: true,
@@ -38,9 +38,18 @@ export class AppointmentNewComponent implements OnInit {
 
     formatter = (x: { firstName: string, lastName: string }) => x.firstName + ' ' + x.lastName;
 
-    constructor(private authService: AuthService,
+    constructor(private formBuilder: FormBuilder,
+        private authService: AuthService,
         private phonebookService: PhonebookService) {
-        this.appointment = new Appointment();
+
+        this.appointmentGroup = this.formBuilder.group({
+            reason: this.formBuilder.control(''),
+            client: this.formBuilder.control(''),
+            doctor: this.formBuilder.control(''),
+            startDate: this.formBuilder.control( moment().toDate()),
+            endDate: this.formBuilder.control( moment().add(1, 'hours').toDate())
+        });
+
     }
 
     ngOnInit() {
@@ -53,9 +62,5 @@ export class AppointmentNewComponent implements OnInit {
         return _.some(this.roles, function (_role) {
             return _role === role;
         });
-    }
-
-    appointmentChange(date) {
-        this.changeAppointment.emit(this.appointment);
     }
 }
--- src/admin/appointment/appointment.routing.ts
+++ src/admin/appointment/appointment.routing.ts
@@ -1,5 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AppointmentListComponent } from './appointment.list.component';
+import { AppointmentNewComponent } from './appointment.new.component';
 import { AuthGuard } from '../../auth/auth-guard.service';
 
 const routes: Routes = [
@@ -7,6 +8,11 @@ const routes: Routes = [
         canActivate: [AuthGuard],
         component: AppointmentListComponent,
         path: 'list'
+    },
+    {
+        canActivate: [AuthGuard],
+        component: AppointmentNewComponent,
+        path: 'new'
     }
 ];
 
